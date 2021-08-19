9260756f8871ccdeb701e05440db2b9d01f5649f
Joey Tran <duytran.arch@gmail.com>
2021-08-19 12:53:15 +0700

    update

--- tvxdhcm_vr/public/app4.js
+++ tvxdhcm_vr/public/app4.js
@@ -8,21 +8,19 @@ import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/thre
 import { ColladaLoader } from './libs/three/loaders/ColladaLoader.js';
 
 class App{
-    
-	constructor(){
+    constructor(folder_,prefix_){
+        this.folder = folder_
+        this.prefix = prefix_        
         let elf;
         var mesh, curMesh;
         var imgNum = 0;
-        var maxImgNum = 21;       
-
+        var maxImgNum = 21;
         var posX = 30;
         var posY = 1.6;
         var posZ = 70;
         this.walkspeed = 5;
-
 		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-        
+		document.body.appendChild( container );        
         this.clock = new THREE.Clock();
         //camera
 		this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 20000 );		
@@ -35,33 +33,25 @@ class App{
         // const light = new THREE.DirectionalLight( 0xffffff );
         // light.position.set( 1, 1, 1 ).normalize();
         // light.intensity = 0.5;
-		// this.scene.add( light );
-        
+		// this.scene.add( light );        
 		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
-        
-		container.appendChild( this.renderer.domElement );
-        
+        this.renderer.outputEncoding = THREE.sRGBEncoding;        
+		container.appendChild( this.renderer.domElement );        
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
         this.controls.target.set(0, 4, 0);
-        this.controls.update();
-        
-        this.stats = new Stats();
-        
+        this.controls.update();        
+        this.stats = new Stats();        
         this.raycaster = new THREE.Raycaster();
         this.workingMatrix = new THREE.Matrix4();
         this.workingVector = new THREE.Vector3();
-        this.origin = new THREE.Vector3();
-        
-        // this.initScene(imgNum,mesh);
-        
-        var path = "./pano/"
-        var name = path+"CS1_ ("+imgNum+").jpg";
+        this.origin = new THREE.Vector3();        
+        // this.initScene(imgNum,mesh);        
+        var path = "./"+this.folder+"/"
+        var name = "./"+this.folder+"/"+this.prefix+"_ ("+imgNum+").jpg";
         // var imgName = document.getElementById('imgName');
         // imgName.textContent = name;
-
         var geometry = new THREE.SphereBufferGeometry( 500, 60, 40 );
         geometry.scale( - 1, 1, 1 );
         var texture = new THREE.TextureLoader().load(name);
@@ -69,16 +59,13 @@ class App{
         mesh = new THREE.Mesh( geometry, material );
         curMesh = mesh;
         this.scene.add(mesh);
-
         // var bNext = document.getElementById('bnxt')
         // var bPrev = document.getElementById('bprv')
         // bNext.addEventListener('mousedown', nextImage, false);
         // bPrev.addEventListener('mousedown', prevImage, false);
 
-        this.setupVR();
-        
-        window.addEventListener('resize', this.resize.bind(this) );
-        
+        this.setupVR();        
+        window.addEventListener('resize', this.resize.bind(this) );        
         this.renderer.setAnimationLoop( this.render.bind(this));
         // animate();
         function nextImage()
@@ -116,20 +103,14 @@ class App{
             this.scene.add(newmesh);
             curMesh = newmesh;
         }
-	}
-    	
-    
-    
+	} 
     random( min, max ){
         return Math.random() * (max-min) + min;
     }
-
     initScene(imgNum,mesh,curMesh){
-
         var maxImgNum = 21;
         var path = "./pano/"
         var name = path+"CS1_ ("+imgNum+").jpg";
-
         var geometry = new THREE.SphereBufferGeometry( 500, 60, 40 );
         geometry.scale( - 1, 1, 1 );
         var texture = new THREE.TextureLoader().load(name);
@@ -137,9 +118,7 @@ class App{
         mesh = new THREE.Mesh( geometry, material );
         curMesh = mesh;
         this.scene.add(mesh);
-    } 
-
-    
+    }
     setupVR(){
 
         this.renderer.xr.enabled = true;        
@@ -185,8 +164,7 @@ class App{
         this.camera.updateProjectionMatrix();
         this.renderer.setSize( window.innerWidth, window.innerHeight );  
     }
-    
-	render() { 
+    render() { 
         // const dt = this.clock.getDelta();
         // this.stats.update();
         // // this.dolly.position.set( 500, 1.6, 500 );
